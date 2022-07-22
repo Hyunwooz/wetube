a5476c6fce819a02c187c7709a31f710e385d7d6
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2022-07-22 16:32:35 +0900

    save point 07.22 16:32

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { edit,remove,see,logout } from "../controller/userController";
+import { edit,remove,see,logout,startGithubLogin,finishGithubLogin } from "../controller/userController";
 
 const userRouter = express.Router();
 
@@ -7,5 +7,7 @@ userRouter.get("logout", logout)
 userRouter.get(":id", see);
 userRouter.get("/edit", edit);
 userRouter.get("/delete", remove);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 
 export default userRouter;
